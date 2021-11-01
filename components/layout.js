6f499a8b80573d3da5f8937c6614a27a3b8aa912
SneK152 <kakanisnehil@gmail.com>
2021-10-31 21:31:05 -0700

    setting up base project

---
+++ components/layout.js
@@ -0,0 +1,24 @@
+import Head from "next/head"
+import Link from "next/link"
+
+export default function Layout({ children }) {
+    return (
+        <div>
+            <Head>
+                <meta charset="UTF-8" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta name="description" content="An open-source initiative to provide accessible and interactive learning for underprivileged kids through a web app." />
+                <meta name="robots" content="index, follow" />
+                <meta property="og:type" content="website" />
+                <meta name="og:title" property="og:title" content="The STEAM Force" />
+                <meta name="og:description" property="og:description" content="An open-source initiative to provide accessible and interactive learning for underprivileged kids through a web app." />
+                <meta property="og:site_name" content="The STEAM Force" />
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:title" content="The STEAM Force" />
+                <meta name="twitter:description" content="An open-source initiative to provide accessible and interactive learning for underprivileged kids through a web app." />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            </Head>
+            {children}
+        </div>
+    )
+}
---
+++ pages/[error].js
@@ -0,0 +1,9 @@
+import Layout from "../components/layout";
+
+export default function Error() {
+    return (
+        <Layout>
+            <h1>404 error</h1>
+        </Layout>
+    )
+}
