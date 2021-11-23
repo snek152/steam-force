9be2ee7a24b15212ae0558027b34f827246aa042
snek152 <kakanisnehil@gmail.com>
2021-11-22 17:43:15 -0800

    adding more edit functionality

--- pages/account/edit.js
+++ pages/account/edit.js
@@ -3,22 +3,58 @@ import { useAuth } from "../../components/userContext"
 import db, { auth, storage } from "../../components/clientApp"
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage"
 import { doc, updateDoc } from "@firebase/firestore"
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
+import { updateEmail, updatePassword } from "@firebase/auth"
 
 export default function EditAccount() {
     const image = useRef(null)
+    const username = useRef(null)
+    const email = useRef(null)
+    const password = useRef(null)
     const user = useAuth()
-    const uploadImage = () => {
-        const storageRef = ref(storage, user.username)
-        uploadBytes(storageRef, image.current.files[0]).then((snapshot) => {
-            getDownloadURL(storageRef).then(async (url) => {
-                await updateDoc(doc(db, "users", user.uid), {
-                    profileUrl: url
+    const [tempImage, setTemp] = useState(user.profileUrl)
+    useEffect(() => {
+        setTemp(user.profileUrl)
+    }, [user.profileUrl])
+    const imageRead = (e) => {
+        let reader = new FileReader()
+        reader.onloadend = () => {
+            setTemp(reader.result)
+        }
+        reader.readAsDataURL(e.target.files[0])
+    }
+    const changeCreds = async (e) => {
+        e.preventDefault()
+        if (image.current.files.length > 0) {
+            const storageRef = ref(storage, `images/${user.username}`)
+            uploadBytes(storageRef, image.current.files[0]).then((snapshot) => {
+                getDownloadURL(storageRef).then(async (url) => {
+                    await updateDoc(doc(db, "users", user.uid), {
+                        profileUrl: url
+                    })
+                    await auth.currentUser.getIdToken(true)
+                    setTemp(user.profileUrl)
                 })
-                auth.currentUser.getIdToken(true)
-
             })
-        })
+        }
+        if (username.current.value !== user.username && username.current.value !== "" && !username.current.value.includes(" ")) {
+            await updateDoc(doc(db, "users", user.uid), {
+                username: username.current.value
+            })
+            await auth.currentUser.getIdToken(true)
+        }
+        if (email.current.value !== auth.currentUser.email && email.current.value !== "" && !email.current.value.includes(" ")) {
+            updateEmail(auth.currentUser, email.current.value).then(() => {
+                console.log("testing updated!!!!!")
+            })
+            await auth.currentUser.getIdToken(true)
+        }
+        if (password.current.value !== "" && !password.current.value.includes(" ")) {
+            updatePassword(auth.currentUser, password.current.value).then(() => {
+                console.log("password updated!!!!!")
+            })
+            await auth.currentUser.getIdToken(true)
+        }
     }
     return <Layout title="Edit Profile">
         <div>
@@ -93,18 +129,23 @@ export default function EditAccount() {
                     </div>
                 </div>
                 <div className="mt-5 md:mt-0 md:col-span-2">
-                    <form action="#" method="POST">
+                    <form onSubmit={e => changeCreds(e)}>
                         <div className="shadow overflow-hidden sm:rounded-md">
                             <div className="px-4 py-5 bg-white sm:p-6">
                                 <div className="grid grid-cols-6 gap-6">
                                     <div className="col-span-6 sm:col-span-3">
                                         <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">Username</label>
-                                        <input type="text" name="first-name" id="first-name" autoComplete="username" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                                        <input ref={username} type="text" name="first-name" id="first-name" autoComplete="username" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
                                     </div>
 
                                     <div className="col-span-6 sm:col-span-3">
                                         <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">Email address</label>
-                                        <input type="text" name="email-address" id="email-address" autoComplete="email" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                                        <input ref={email} type="text" name="email-address" id="email-address" autoComplete="email" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+                                    </div>
+
+                                    <div className="col-span-6 sm:col-span-3">
+                                        <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+                                        <input ref={password} type="password" name="password" id="password" autoComplete="email" className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
                                     </div>
 
                                 </div>
@@ -114,13 +155,13 @@ export default function EditAccount() {
                                     </label>
                                     <div className="mt-1 flex items-center">
                                         <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                                            {user.profileUrl ? <img className="h-full w-full text-gray-300" src={user.profileUrl} alt="" /> : <img className="h-full w-full text-gray-300" src="/avatar.svg" alt="" />}
+                                            {user.profileUrl ? <img className="h-full w-full text-gray-300" src={tempImage} alt="" /> : <img className="h-full w-full text-gray-300" src="/avatar.svg" alt="" />}
                                         </span>
                                         <label htmlFor="file-upload" className="cursor-pointer">
                                             <span className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                                 Change
                                             </span>
-                                            <input id="file-upload" name="file-upload" type="file" accept="image/*" className="sr-only" onChange={e => uploadImage(e)} ref={image} />
+                                            <input id="file-upload" name="file-upload" type="file" accept="image/*" className="sr-only" ref={image} onChange={imageRead}/>
                                         </label>
                                     </div>
                                 </div>
