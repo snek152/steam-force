739aa51faed1bbc79c4e8fdfc95b85a360ac4c19
snek152 <kakanisnehil@gmail.com>
2021-12-08 10:19:40 -0800

    more ui change

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,9 +6,9 @@ export default function Home() {
         <Layout title="Home">
             <div className="relative flex h-[calc(100vh-64px)] w-full items-center">
                 <div className="relative z-[100] p-8 flex-grow bg-white bg-opacity-70">
-                    <h1 className="text-center text-5xl text-black">The Steam Force</h1>
+                    <h1 className="text-center text-5xl text-black font-semibold">The Steam Force</h1>
                     <hr className="bg-black border-none h-[0.1rem] rounded-xl bg-opacity-50 m-2" />
-                    <h2 className="text-center text-xl text-blue">An open source initiative for interactive and sustainable education</h2>
+                    <h2 className="text-center text-xl text-blue-500">An open source initiative for interactive and sustainable education</h2>
                 </div>
                 <div>
                     <Image layout="fill" src="/home.png" priority quality="85" className="object-cover bg-center absolute z-50" />
