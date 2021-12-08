06297360518f44f8e4dd399266c49b079b2e9198
snek152 <kakanisnehil@gmail.com>
2021-12-08 15:12:47 -0800

    small ui change again

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,14 +4,16 @@ import Image from "next/image"
 export default function Home() {
     return (
         <Layout title="Home">
-            <div className="relative flex h-[calc(100vh-64px)] w-full items-center">
-                <div className="relative z-[100] p-8 flex-grow bg-white bg-opacity-70">
-                    <h1 className="text-center text-5xl text-black font-semibold">The Steam Force</h1>
-                    <hr className="bg-black border-none h-[0.1rem] rounded-xl bg-opacity-50 m-2" />
-                    <h2 className="text-center text-xl text-blue-500">An open source initiative for interactive and sustainable education</h2>
+            <div className="relative h-[calc(100vh-64px)] w-full bg-white bg-opacity-70">
+                <div className="bg-white relative bg-opacity-50 h-full items-center z-[200] flex">
+                    <div className="relative z-[120] p-8 flex-grow">
+                        <h1 className="text-center text-5xl text-black font-semibold">The Steam Force</h1>
+                        <hr className="bg-black border-none h-[0.1rem] rounded-xl bg-opacity-50 m-2 w-[30rem] mx-auto" />
+                        <h2 className="text-center text-xl text-blue-600">An open source initiative for interactive and sustainable education</h2>
+                    </div>
                 </div>
-                <div>
-                    <Image layout="fill" src="/home.png" priority quality="85" className="object-cover bg-center absolute z-50" />
+                <div className="z-50">
+                    <Image layout="fill" src="/home.png" priority loading="eager" quality="85" className="object-cover bg-center absolute z-50" />
                 </div>
             </div>
         </Layout>
