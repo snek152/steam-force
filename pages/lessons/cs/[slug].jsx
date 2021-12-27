9770a24a09d0d6628e866d9343560bccffc5914a
snek152 <kakanisnehil@gmail.com>
2021-12-27 15:54:57 -0800

    adding simple question system

--- pages/lessons/cs/[slug].jsx
+++ pages/lessons/cs/[slug].jsx
@@ -11,11 +11,16 @@ import { useRouter } from "next/router"
 import { doc, updateDoc } from "firebase/firestore"
 import { useAuth } from "../../../components/userContext"
 import db from "../../../components/clientApp"
-
+import $ from "jquery"
 
 export default function CSLesson({ data, content, lessons }) {
     const router = useRouter()
     const user = useAuth()
+    const formSubmit = async (e) => {
+        e.preventDefault()
+        const val = $(`input[name=${data.slug}-question]:checked + label`).html()
+        $(`:has(> input[name=${data.slug}-question]:checked)`).addClass("bggreen")
+    }
     useEffect(() => {
         const fn = async () => {
             try {
@@ -40,6 +45,18 @@ export default function CSLesson({ data, content, lessons }) {
                 <h1>{data.heading}</h1>
                 <p className="mb-0">In this section you will {data.desc}.</p>
                 <div dangerouslySetInnerHTML={{ __html: content }}></div>
+                <div className="bg-gray-50 shadow-md rounded-md p-3 mb-5">
+                    <span className="font-semibold text-lg">Review: {data.question}</span>
+                    <form onSubmit={formSubmit}>
+                        {data.answerchoices.map(choice => (
+                            <span className="flex flex-row flex-grow m-1 p-1 rounded-lg">
+                                <input type="radio" id={choice} name={`${data.slug}-question`} className="h-4 w-4 focus:outline-none focus:ring-transparent align-middle mt-[6px] mr-2" />
+                                <label className="not-prose inline-block align-baseline" htmlFor={choice}>{choice}</label>
+                            </span>
+                        ))}
+                        <button type="submit" className="bg-blue-500 text-white py-1 px-2 rounded-md shadow-lg">Submit</button>
+                    </form>
+                </div>
                 <div className="flex justify-between h-10">
                     <Link href={`/lessons/cs/${data.prev}`}>
                         <a className={`bg-gray-300 rounded-md shadow-md no-underline pt-[0.4rem] px-3 font-light ${data.prev ? "" : "pointer-events-none bg-gray-200 text-gray-400"}`}>
