6ad93b106b74721938cc32bbad7fc2da3d3a1a72
snek152 <kakanisnehil@gmail.com>
2022-01-15 18:10:27 -0800

    tweaking scrollbar and form

--- pages/account/contribute.jsx
+++ pages/account/contribute.jsx
@@ -15,8 +15,7 @@ export default function AdminPanel() {
     const answer4 = useRef(null)
     const correct = useRef(null)
     const unit = useRef(null)
-    const [content, setContent] = useState(null)
-    const [preview, setPreview] = useState(null)
+    const content = useRef(null)
     const formSubmit = async (e) => {
         e.preventDefault()
         await fetch(`${process.env.NODE_ENV === "development" ? "localhost:3000" : "steam-force.vercel.app"}/api/addcourse`, {
@@ -26,42 +25,32 @@ export default function AdminPanel() {
             })
         })
     }
-    useEffect(() => {
-        const fn = async () => {
-            try {
-                const file = await markdownToHtml(content?.toString())
-                setPreview(file)
-            } catch { }
-        }
-        fn()
-    }, [content])
-    useEffect(() => {
-        $("textarea").each(function () {
-            this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;");
-        }).on("input", function () {
-            this.style.height = "auto";
-            this.style.height = (this.scrollHeight) + "px";
-        });
-    }, [])
-    return <Layout title="Contribute Content">
-        <form onSubmit={formSubmit}>
-            <InputField labelName="Title" ref={title} name="title" type="text" />
-            <InputField labelName="Heading" ref={heading} name="heading" type="text" />
-            <InputField labelName="Description" ref={desc} name="desc" type="text" />
-            <div className="flex flex-1">
-                <textarea value={content} onChange={(e) => setContent(e.target.value)} placeholder="Lesson Content" className="w-1/2 appearance-none h-full rounded-md relative inline-block px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" />
-                <div className="w-1/2 inline-block">
-                    <h1>Preview</h1>
-                    <div className="prose" dangerouslySetInnerHTML={{ __html: preview }}></div>
-                </div>
+    return <Layout title="Contribute Content" container={false}>
+        <header className="bg-white shadow">
+            <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                <h1 className="text-3xl font-bold text-gray-900">
+                    Contribute
+                </h1>
             </div>
-            <button type="submit">Submit</button>
-        </form>
+        </header>
+        <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <form onSubmit={formSubmit} className="divide-y-1 space-y-5">
+                <InputField labelName="Title" ref={title} name="title" type="text" />
+                <InputField labelName="Heading" ref={heading} name="heading" type="text" />
+                <InputField labelName="Description" ref={desc} name="desc" type="text" />
+                <span className="inline-block">
+                    <label htmlFor="content" className="text-gray-500 text-sm">Lesson Content</label>
+                    <input accept="text/markdown" ref={content} id="content" name="content" type="file" required hidden />
+                </span>
+                <button type="submit" className="block">Submit</button>
+            </form>
+        </div>
     </Layout>
 }
 
 function InputField({ labelName, ref, name, type }) {
-    return <><label htmlFor={name} className="sr-only">Title</label>
+    return <><label htmlFor={name} className="sr-only">{labelName}</label>
         <input ref={ref} id={name} name={name} type={type} required className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm" placeholder={labelName} />
     </>
 }
+
