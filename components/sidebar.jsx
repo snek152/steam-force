f2babf7aa2cff6ce53cff0a31e1386d7266817df
snek152 <kakanisnehil@gmail.com>
2022-01-16 21:12:22 -0800

    adding modal

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -1,8 +1,8 @@
 import { useAuth } from "./userContext"
-import { Disclosure, Transition } from "@headlessui/react"
+import { Dialog, Disclosure, Transition } from "@headlessui/react"
 import Link from "next/link"
 import { useRouter } from "next/router"
-import { useEffect } from "react"
+import { Fragment, useEffect, useState } from "react"
 import $ from "jquery"
 
 const units = {
@@ -15,6 +15,15 @@ const units = {
 export default function Sidebar({ lessons, type }) {
     const user = useAuth()
     const router = useRouter()
+    const [open, setOpen] = useState(false)
+
+    const openModal = () => {
+        setOpen(true)
+    }
+
+    const closeModal = () => {
+        setOpen(false)
+    }
     useEffect(() => {
         if (router.pathname.includes("science")) {
             $(".science").trigger("click")
@@ -164,6 +173,55 @@ export default function Sidebar({ lessons, type }) {
                         </>
                     )}
                 </Disclosure>
+                <button type="button" onClick={openModal} className="px-4 py-2 text-sm font-medium text-white bg-blue-600 block m-auto my-3 rounded-md bg-opacity-[0.85] hover:bg-opacity-75">
+                    Questions?
+                </button>
+                <Transition appear show={open} as={Fragment}>
+                    <Dialog as="div" className="fixed inset-0 z-[1000] overflow-y-auto" onClose={closeModal}>
+                        <div className="min-h-screen px-4 text-center">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0"
+                                enterTo="opacity-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
+                            >
+                                <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-75" />
+                            </Transition.Child>
+                            <span className="inline-block h-screen align-middle" aria-hidden="true">
+                                &#8203;
+                            </span>
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95"
+                            >
+                                <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                                    <Dialog.Title as="h3"
+                                        className="text-lg font-medium leading-6 text-gray-900">
+                                        Title goes here
+                                    </Dialog.Title>
+                                    <div className="mt-2">
+                                        <p className="text-sm text-gray-500">
+                                            Your payment has been successfully submitted. We've sent you an email with all of the details of your order.
+                                        </p>
+                                    </div>
+                                    <div className="mt-4">
+                                        <button onClick={closeModal} type="button" className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500">
+                                            Close modal
+                                        </button>
+                                    </div>
+                                </div>
+                            </Transition.Child>
+                        </div>
+                    </Dialog>
+                </Transition>
             </div>
         </div>
     </div>
--- pages/account/contribute.jsx
+++ pages/account/contribute.jsx
@@ -1,7 +1,5 @@
 import Layout from "../../components/layout"
 import { useEffect, useRef, useState } from "react"
-import markdownToHtml from "../../components/markdown"
-import $ from "jquery"
 
 export default function AdminPanel() {
     const heading = useRef(null)
@@ -16,19 +14,17 @@ export default function AdminPanel() {
     const correct = useRef(null)
     const unit = useRef(null)
     const contentValue = useRef(null)
-    const [content, setContent] = useState("Lesson Content")
+    const [content, setContent] = useState("Lesson Content (MD format)")
     const formSubmit = async (e) => {
         e.preventDefault()
-        await fetch(`${process.env.NODE_ENV === "development" ? "localhost:3000" : "steam-force.vercel.app"}/api/addcourse`, {
-            method: "POST",
-            body: JSON.parse({
+        console.log("submitted!!")
+        // await fetch(`${process.env.NODE_ENV === "development" ? "localhost:3000" : "steam-force.vercel.app"}/api/addcourse`, {
+        //     method: "POST",
+        //     body: JSON.parse({
 
-            })
-        })
+        //     })
+        // })
     }
-    useEffect(() => {
-        console.log(content)
-    }, [content])
     return <Layout title="Contribute Content" container={false}>
         <header className="bg-white shadow">
             <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -38,20 +34,27 @@ export default function AdminPanel() {
             </div>
         </header>
         <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <form onSubmit={formSubmit} className="divide-y-1 space-y-5">
-                <InputField labelName="Title" ref={title} name="title" type="text" className="w-1/2 inline-block" />
-                <InputField labelName="Heading" ref={heading} name="heading" type="text" className="w-1/2 inline-block" />
+            <form onSubmit={formSubmit} className="space-y-5">
+                <span className="flex space-y-5">
+                    <InputField labelName="Title" ref={title} name="title" type="text" className="mr-2 inline-block" />
+                    <InputField labelName="Heading" ref={heading} name="heading" type="text" className="ml-2 inline-block" />
+                </span>
                 <InputField labelName="Description" ref={desc} name="desc" type="text" />
                 <span className="rounded-md relative block w-full px-1.5 py-1 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm">
                     <label htmlFor="content" className="text-gray-500 text-sm cursor-pointer flex flex-auto items-center">
-                        <span className="shadow rounded-lg bg-blue-400 h-10 inline-flex items-center justify-center w-10">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 stroke-gray-200" fill="none" viewBox="0 0 24 24">
+                        <span className="shadow rounded-lg bg-blue-500 h-10 inline-flex items-center justify-center w-10">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 stroke-white" fill="none" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
                             </svg>
                         </span>
                         <p className="inline-block text-base ml-2 h-full">{content}</p>
                     </label>
                     <input accept="text/markdown" ref={contentValue} onChange={e => setContent(e.target.files[0].name)} id="content" name="content" type="file" required hidden />
+                </span>
+                <InputField labelName="Question" ref={question} name="question" type="text" />
+                <span className="bg-white flex flex-row flex-grow p-1 rounded-md relative w-1/2 px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm">
+                    <input type="radio" id="answer1" name="answer1" className="h-4 w-4 focus:outline-none focus:ring-transparent align-middle mt-[3px] mr-2" />
+                    <input className="inline-block align-baseline text-gray-500" />
                 </span>
                 <button type="submit" className="block">Submit</button>
             </form>
