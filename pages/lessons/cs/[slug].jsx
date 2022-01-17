64475057bdfe5e086a163cc76c7469399a3fa27d
snek152 <kakanisnehil@gmail.com>
2022-01-16 21:37:26 -0800

    adding cursor disabled

--- pages/lessons/cs/[slug].jsx
+++ pages/lessons/cs/[slug].jsx
@@ -78,17 +78,19 @@ export default function CSLesson({ data, content, lessons }) {
                 <h1>{data.heading}</h1>
                 <p className="mb-0">In this section you will {data.desc}.</p>
                 <div dangerouslySetInnerHTML={{ __html: content }}></div>
-                <div className={`bg-gray-50 shadow-md rounded-md p-3 mb-5 ${user.completed?.includes(data.slug) ? "bg-white text-gray-400 pointer-events-none" : ""}`}>
-                    <span className="font-semibold text-lg">Review: {data.question}</span>
-                    <form onSubmit={formSubmit}>
-                        {answerchoices.map(choice => (
-                            <span className={`flex flex-row flex-grow m-1 p-1 rounded-lg ${user.completed?.includes(data.slug) && choice == data.correct ? "bggreen" : ""}`} key={choice}>
-                                <input type="radio" id={choice} name={`${data.slug}-question`} className="h-4 w-4 focus:outline-none focus:ring-transparent align-middle mt-[6px] mr-2" />
-                                <label className="not-prose inline-block align-baseline" htmlFor={choice}>{choice}</label>
-                            </span>
-                        ))}
-                        <button type="submit" className={`text-white py-1 px-2 rounded-md shadow-lg ${user.completed?.includes(data.slug) ? "bg-blue-200" : "bg-blue-500"}`}>Submit</button>
-                    </form>
+                <div className={user.completed?.includes(data.slug) ? "cursor-not-allowed" : ""}>
+                    <div className={`bg-gray-50 shadow-md rounded-md p-3 mb-5 ${user.completed?.includes(data.slug) ? "bg-white text-gray-400 cursor-not-allowed pointer-events-none" : ""}`}>
+                        <span className="font-semibold text-lg">Review: {data.question}</span>
+                        <form onSubmit={formSubmit}>
+                            {answerchoices.map(choice => (
+                                <span className={`flex flex-row flex-grow m-1 p-1 rounded-lg ${user.completed?.includes(data.slug) && choice == data.correct ? "bggreen" : ""}`} key={choice}>
+                                    <input type="radio" id={choice} name={`${data.slug}-question`} className="h-4 w-4 focus:outline-none focus:ring-transparent align-middle mt-[6px] mr-2" />
+                                    <label className="not-prose inline-block align-baseline" htmlFor={choice}>{choice}</label>
+                                </span>
+                            ))}
+                            <button type="submit" className={`text-white py-1 px-2 rounded-md shadow-lg ${user.completed?.includes(data.slug) ? "bg-blue-200" : "bg-blue-500"}`}>Submit</button>
+                        </form>
+                    </div>
                 </div>
                 <div className="flex justify-between h-10">
                     <Link href={`/lessons/cs/${data.prev}`}>
