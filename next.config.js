e96bb024c51137e35ba7cb13bb4c0528e72dca34
Snehil Kakani <kakanisnehil@gmail.com>
2022-02-07 16:53:54 -0800

    changing next config

--- next.config.js
+++ next.config.js
@@ -2,23 +2,23 @@ const withPWA = require("next-pwa")
 const withBundle = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 })
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+  },
+  images: {
+    domains: ["firebasestorage.googleapis.com"],
+  },
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+  },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+}
 
-module.exports = withBundle(
-  withPWA({
-    reactStrictMode: true,
-    env: {
-      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-    },
-    images: {
-      domains: ["firebasestorage.googleapis.com"],
-    },
-    pwa: {
-      dest: "public",
-      disable: process.env.NODE_ENV === "development",
-    },
-    i18n: {
-      locales: ["en"],
-      defaultLocale: "en",
-    },
-  }),
-)
+module.exports = withBundle(withPWA(config))
